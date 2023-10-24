aee60edeff2715962e51e18b7fa868a48b3676c7
fberal <148898675+fberal@users.noreply.github.com>
2023-10-24 20:02:42 +0200

    first exemples of prints

---
+++ print3Broken.hs
@@ -0,0 +1,14 @@
+-- print3Broken.hs
+module Print3Broken where
+
+greeting :: String
+greeting = "Yarrrrr"
+
+printSecond :: IO ()
+printSecond = do
+  putStrLn greeting
+
+main :: IO ()
+main = do
+  putStrLn greeting
+  printSecond
